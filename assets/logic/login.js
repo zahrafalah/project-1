17c053be5c90689de90e9b70b3776e663b6c2ac6
unknown <webd3vdave@gmail.com>
2018-03-18 11:24:42 -0700

    added login.js file & html file with sample signup & login forms. signup form validates minimum PIN code length, login form correctly identifies PIN code for username entered. still need to add check of database to prevent duplicate user names.

---
+++ assets/logic/login.js
@@ -0,0 +1,146 @@
+
+
+var loginState="loggedOut";
+var userName;
+var pinCode;
+var addDate;
+
+
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAqqFDGVm6pNyj_skReV42OfQEXyG3G5iM",
+    authDomain: "logintest-2d079.firebaseapp.com",
+    databaseURL: "https://logintest-2d079.firebaseio.com",
+    projectId: "logintest-2d079",
+    storageBucket: "logintest-2d079.appspot.com",
+    messagingSenderId: "72723334125"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+console.log("db connection successful");
+
+//add new user to the database//
+
+$("#add-user-btn").on("click", function(event) {
+  event.preventDefault();
+
+  // Get user input
+  var userName = $("#username-input").val().trim();
+  var pinCode = $("#pin-input").val().trim();
+  var addDate = moment().format("MM/DD/YYYY");
+  var loginState = "loggedIn";
+
+  // Create local temp object for holding new user data
+  var newUser = {
+    userName: userName,
+    pinCode: pinCode,
+    addDate: addDate,  
+    loginState : loginState 
+  };
+  //user input validation
+
+  if (pinCode.length < 4) {
+    $("#pinError").text("Your PIN must be at least 4 characters.");
+    $("#pin-input").val("");
+  }
+  else {
+  // Uploads employee data to the database
+  database.ref("users/" + userName).set(newUser);
+
+  // Logs everything to console
+  console.log(newUser.userName);
+  console.log(newUser.pinCode);
+  console.log(addDate);
+  
+
+  // Alert
+  alert("Welcome to Unborable!");
+
+  // Clears all of the text-boxes
+  $("#username-input").val("");
+  $("#pin-input").val("");
+  $("#pinError").text("");
+  };
+});
+
+
+
+//user login - check the database for 
+
+$("#login-btn").on("click", function(event) {
+    event.preventDefault();
+  
+    // Get user input
+    var userName = $("#login-username").val().trim();
+    var pinCode = $("#login-pin").val().trim().toString();
+    console.log("un"+userName);
+  
+    // get back pincode from database where record equals userName input value from the form
+    
+    database.ref("/users/"+userName).once("value").then(function(snapshot) {
+        console.log("un"+userName);
+        var userRecord = snapshot.val();
+        console.log(snapshot.val());
+    //    var userNm = snapshot.child("users").userName;
+        var key = snapshot.child.key;
+        var resultPinCode=snapshot.child("pinCode").val().toString();
+
+        console.log("pinCode: "+resultPinCode);
+        console.log("pincode input: " +pinCode);
+        if (resultPinCode==pinCode) {
+
+          console.log("match");
+          $("#loginError").text("That's a match! Welcome back.");
+          loginState="loggedIn";
+    //      $("#loginError").text("");
+          console.log("usernm/userstate: " + userName, loginState)
+          //NEXT--DO SOMETHING - QUERY THE DATABASE AND RETURN THE USER'S SAVED ITEMS 
+          //FOR EACH ITEM IN RESULT SET, APPEND AN 'EVENT INFO CARD' object TO THE INTO THE SAVED ITEMS SECTION
+          
+           
+        }
+        else {
+             $("#loginError").text("That User ID/PIN combo does not match any in our records. Try again.");
+             $("#login-pin").text("");
+            loginState="notLoggedIn";
+            console.log("usernm/userstate: " + userName, loginState)
+
+        };
+
+    });
+    
+    
+});
+  //log out function - NOT SURE WE NEED THIS REALLY
+
+  // $("#logout-btn").on("click", function(event) {
+  //   console.log(userName);
+  //   event.preventDefault();
+    
+  //   var update = {
+     
+  //     loginState : "loggedOut"
+  //   };
+  //   database.ref("/users/"+userName).update(update);
+  // });
+
+
+
+    //user input validation
+  
+    
+  
+    // Clears all of the text-boxes
+    $("#username-input").val("");
+    $("#pin-input").val("");
+    $("#pinError").text("");
+    $("#login-username").val("");
+    $("#login-pin").val("");
+    $("#loginError").text("");
+   
+ 
+  
