e919e8465d38c9a2ffc201942cf14573ad8b24b8
Zahra Falah <34927820+zahrafalah@users.noreply.github.com>
2018-03-27 20:37:22 -0700

    Create social.js

---
+++ social.js
@@ -0,0 +1,12 @@
+function tweetCardInfo() {
+	        var twitterURL = "https://twitter.com/intent/tweet?text=" + CardInfo;
+	        var CardInfo = $("#card").text();
+}
+        
+function tweetCardInfo() {
+          var cardInfo = cardObject.title;
+          var tweetBtn = $('<a></a>').addClass('twitter-share-button').attr('href', 'http://twitter.com/share').attr('data-url', cardObject.imageLinks.thumbnail).attr('data-text', authors + bookObject.title);
+          $('.card-body').append(tweetBtn);
+          twttr.widgets.load();
+}
+ tweeCardInfo();
