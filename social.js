52d596fd7e1031836fc9777bb8881a4c8c25b86b
Zahra Falah <34927820+zahrafalah@users.noreply.github.com>
2018-03-31 12:26:41 -0700

    Update social.js

--- social.js
+++ social.js
@@ -1,12 +1,22 @@
-function tweetCardInfo() {
-	        var twitterURL = "https://twitter.com/intent/tweet?text=" + CardInfo;
-	        var CardInfo = $("#card").text();
-}
+// function tweetCardInfo() {
+// 	        var twitterURL = "https://twitter.com/intent/tweet?text=" + CardInfo;
+// 	        var CardInfo = $("#card").text();
+// }
         
-function tweetCardInfo() {
-          var cardInfo = cardObject.title;
-          var tweetBtn = $('<a></a>').addClass('twitter-share-button').attr('href', 'http://twitter.com/share').attr('data-url', cardObject.imageLinks.thumbnail).attr('data-text', authors + cardObject.title);
+// function tweetCardInfo() {
+//           var cardInfo = cardObject.title;
+//           var tweetBtn = $('<a></a>').addClass('twitter-share-button').attr('href', 'http://twitter.com/share').attr('data-url', cardObject.imageLinks.thumbnail).attr('data-text', authors + cardObject.title);
+//           $('.card-body').append(tweetBtn);
+//           twttr.widgets.load();
+// }
+//  tweeCardInfo();
+    function tweetCardInfo() {
+          console.log("this one also");
+          var cardInfo = eventItem.title;
+          var twitterURL = "https://twitter.com/intent/tweet?text=" + CardInfo;
+	        // var CardInfo = $("#card").text();          
+          var tweetBtn = $('<a></a>').addClass('twitter-share-button').attr('href', 'http://twitter.com/share').attr('data-url', eventItem.imageURL).attr('data-text',eventItem.title);
           $('.card-body').append(tweetBtn);
           twttr.widgets.load();
-}
- tweeCardInfo();
+        }
+        tweetCardInfo();
